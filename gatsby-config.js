74787734c7050d4ced6446828b217db215257b32
sophie <s.hautcoeur@gmail.com>
2020-03-29 17:10:00 +0300

    gatsby blog initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,9 +11,18 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
-      },
+      }
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdowns`,
+        path: `${__dirname}/src/markdown-pages`,
+      }
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
